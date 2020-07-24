c05e098aad85673f811ee4ad8d7f2a56b6f3c233
Ali havasi <alihvs@gmail.com>
2020-07-25 00:07:24 +0430

    feat: add FA to i18n

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -6,6 +6,7 @@ import ES from './es-ES.json'
 import FR from './fr.json'
 import RU from './ru.json'
 import DE from './de.json'
+import FA from './fa-IR.json'
 
 const i18nMap = {
   'zh': ZHCN,
@@ -16,6 +17,7 @@ const i18nMap = {
   'fr': FR,
   'ru': RU,
   'de': DE,
+  'fa-IR': FA,
 }
 
 export default function (language) {
